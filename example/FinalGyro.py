49c8833569e92365a8510779a43249d2bde75d08
zasaifee <zasa2720@colorado.edu>
2024-06-24 14:45:33 -0600

    Comments for the File, Changes made 6/24/24

--- example/FinalGyro.py
+++ example/FinalGyro.py
@@ -1,3 +1,12 @@
+
+"""
+This code goes over the MPU6050 gyro/accelerometer functionalities to determine the Yaw, Pitch, Roll.
+Originally this code was written for the Arduino Module but this code was transcribed to be used for 
+the RaspberryPi written in Python.
+
+https://howtomechatronics.com/tutorials/arduino/arduino-and-mpu6050-accelerometer-and-gyroscope-tutorial/
+"""
+
 import smbus2 as smbus
 import time
 import math
@@ -21,8 +30,10 @@ GyroErrorX, GyroErrorY, GyroErrorZ = 0, 0, 0
 elapsedTime, currentTime, previousTime = 0, time.time(), 0
 c = 0
 
-# Function to read MPU6050 data
 def read_mpu6050_data(register):
+    """
+    Function to read MPU6050 data.
+    """
     high = bus.read_byte_data(MPU, register)
     low = bus.read_byte_data(MPU, register+1)
     value = ((high << 8) | low)
@@ -30,8 +41,10 @@ def read_mpu6050_data(register):
         value = value - 65536
     return value
 
-# Function to calculate IMU error
 def calculate_IMU_error():
+    """
+    Function to calculate IMU error.
+    """
     global AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ
     c = 0
     while (c < 200):
@@ -68,33 +81,38 @@ bus.write_byte_data(MPU, 0x6B, 0x00)
 # Call this function if you need to get the IMU error values for your module
 calculate_IMU_error()
 
-while True:
-    # === Read accelerometer data === //
-    AccX = read_mpu6050_data(ACCEL_XOUT_H) / 16384.0
-    AccY = read_mpu6050_data(ACCEL_XOUT_H+2) / 16384.0
-    AccZ = read_mpu6050_data(ACCEL_XOUT_H+4) / 16384.0
-    # Calculating Roll and Pitch from the accelerometer data
-    accAngleX = (math.atan(AccY / math.sqrt(math.pow(AccX, 2) + math.pow(AccZ, 2))) * 180 / math.pi) - 0.58
-    accAngleY = (math.atan(-1 * AccX / math.sqrt(math.pow(AccY, 2) + math.pow(AccZ, 2))) * 180 / math.pi) + 1.58
-    # === Read gyroscope data === //
-    previousTime = currentTime
-    currentTime = time.time()
-    elapsedTime = currentTime - previousTime
-    GyroX = read_mpu6050_data(GYRO_XOUT_H) / 131.0
-    GyroY = read_mpu6050_data(GYRO_XOUT_H+2) / 131.0
-    GyroZ = read_mpu6050_data(GYRO_XOUT_H+4) / 131.0
-    # Correct the outputs with the calculated error values
-    GyroX = GyroX + 0.56
-    GyroY = GyroY - 2
-    GyroZ = GyroZ + 0.79
-    # Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by seconds (s) to get the angle in degrees
-    gyroAngleX = gyroAngleX + GyroX * elapsedTime
-    gyroAngleY = gyroAngleY + GyroY * elapsedTime
-    yaw =  yaw + GyroZ * elapsedTime
-    # Complementary filter - combine accelerometer and gyro angle values
-    roll = 0.96 * gyroAngleX + 0.04 * accAngleX
-    pitch = 0.96 * gyroAngleY + 0.04 * accAngleY
-    # Print the values
-    print("Roll: ", roll, " Pitch: ", pitch, " Yaw: ", yaw)
-    time.sleep(0.09)
+def Gyro():
+    """
+    Function to calculate Gyro values and print them consistently
+    """
+    while True:
+        # === Read accelerometer data === //
+        AccX = read_mpu6050_data(ACCEL_XOUT_H) / 16384.0
+        AccY = read_mpu6050_data(ACCEL_XOUT_H+2) / 16384.0
+        AccZ = read_mpu6050_data(ACCEL_XOUT_H+4) / 16384.0
+        # Calculating Roll and Pitch from the accelerometer data
+        accAngleX = (math.atan(AccY / math.sqrt(math.pow(AccX, 2) + math.pow(AccZ, 2))) * 180 / math.pi) - 0.58
+        accAngleY = (math.atan(-1 * AccX / math.sqrt(math.pow(AccY, 2) + math.pow(AccZ, 2))) * 180 / math.pi) + 1.58
+        # === Read gyroscope data === //
+        previousTime = currentTime
+        currentTime = time.time()
+        elapsedTime = currentTime - previousTime
+        GyroX = read_mpu6050_data(GYRO_XOUT_H) / 131.0
+        GyroY = read_mpu6050_data(GYRO_XOUT_H+2) / 131.0
+        GyroZ = read_mpu6050_data(GYRO_XOUT_H+4) / 131.0
+        # Correct the outputs with the calculated error values
+        GyroX = GyroX + 0.56
+        GyroY = GyroY - 2
+        GyroZ = GyroZ + 0.79
+        # Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by seconds (s) to get the angle in degrees
+        gyroAngleX = gyroAngleX + GyroX * elapsedTime
+        gyroAngleY = gyroAngleY + GyroY * elapsedTime
+        yaw =  yaw + GyroZ * elapsedTime
+        # Complementary filter - combine accelerometer and gyro angle values
+        roll = 0.96 * gyroAngleX + 0.04 * accAngleX
+        pitch = 0.96 * gyroAngleY + 0.04 * accAngleY
+        # Print the values
+        print("Roll: ", roll, " Pitch: ", pitch, " Yaw: ", yaw)
+        time.sleep(0.09)
+
 
