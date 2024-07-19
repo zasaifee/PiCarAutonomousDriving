9fe261ff8c201f54f6428fdff4f845c4d4642127
Stephanie Torres <stephanie.torres-1@colorado.edu>
2024-07-19 14:26:22 -0600

    Updated GyroKalmanFilter.py with the Multiprocessing and StraightLineLoop

--- example/GyroKalmanFilter.py
+++ example/GyroKalmanFilter.py
@@ -87,7 +87,7 @@ bus.write_byte_data(MPU, 0x6B, 0x00)
 # Call this function if you need to get the IMU error values for your module
 calculate_IMU_error()
 
-def readGyro():
+def readGyro(yaw):
     global currentTime, GyroX, GyroY, GyroZ, bias_z, angle_z, P_angle_z, P_bias_z, Q_angle_z, gyroAngleX, gyroAngleY, accAngleX, accAngleY, roll, pitch
     while True:
         # === Read accelerometer data === //
@@ -128,7 +128,8 @@ def readGyro():
         # Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by seconds (s) to get the angle in degrees
         gyroAngleX = gyroAngleX + GyroX * elapsedTime # Update gyroscope x angle
         gyroAngleY = gyroAngleY + GyroY * elapsedTime # Update gyroscope y angle
-        yaw =  angle_z  # Use the angle estimated by the Kalman filter
+        #yaw =  angle_z  # Use the angle estimated by the Kalman filter
+        yaw.value = angle_z
 
         # Complementary filter - combine accelerometer and gyro angle values
         roll = 0.96 * gyroAngleX + 0.04 * accAngleX
@@ -138,5 +139,3 @@ def readGyro():
         # print("Roll: ", roll, " Pitch: ", pitch, " Yaw: ", yaw)
         
         time.sleep(0.10)
-
-readGyro()
